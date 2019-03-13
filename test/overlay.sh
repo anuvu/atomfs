9185aaa54123d8fe2ba4f634f720834ca88cbc66
Tycho Andersen <tycho@tycho.ws>
2019-03-13 09:08:25 -0600

    check that whiteouts work in lowerdirs
    spoiler alert: they don't
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- test/overlay.sh
+++ test/overlay.sh
@@ -26,6 +26,14 @@ function cleanup() {
 
 trap cleanup EXIT HUP INT TERM
 
+# do some custom setup to test whiteouts in lowerdirs
+touch layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddenfile
+mknod layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddenfile c 0 0
+
+mkdir layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddendir
+mkdir layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddendir
+setfattr -n trusted.overlay.opaque -v y layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddendir
+
 mkdir -p target
 mkdir -p work
 mkdir -p upper
@@ -33,8 +41,12 @@ mount -t overlay overlay "-olowerdir=${lowerdirs},upperdir=upper,workdir=work" t
 rmdir upper
 
 for i in $(seq ${levels}); do
-    stat target/${i} >& /dev/null || (echo "${i} missing" && exit 1)
+    stat target/${i} || (echo "${i} missing" && exit 1)
 done
+
+# check our special whiteouts
+[ ! -f target/hiddenfile ]
+[ ! -d target/hiddendir ]
 
 touch target/writable
 
