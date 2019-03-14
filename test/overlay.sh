3921b00aa115070fd6a66dea8b6474ae1f25c3b4
Tycho Andersen <tycho@tycho.ws>
2019-03-14 15:21:32 -0600

    overlay test: put whiteouts in the right order
    whoops, just kidding, they do work! you just have to put things in the
    right order :)
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- test/overlay.sh
+++ test/overlay.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+RESULT=failure
+
 levels=10
 
 lowerdirs=""
@@ -17,28 +19,35 @@ done
 lowerdirs=${lowerdirs::-1}
 
 function cleanup() {
+    set +x
     umount target || true
     rm -rf target work upper || true
     for i in $(seq ${levels}); do
         rm -rf layer${i}* || true
     done
+
+    echo ==================
+    echo RESULT: $RESULT
+    echo ==================
 }
 
 trap cleanup EXIT HUP INT TERM
 
 # do some custom setup to test whiteouts in lowerdirs
-touch layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddenfile
-mknod layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddenfile c 0 0
+touch layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddenfile
+mknod layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddenfile c 0 0
 
 mkdir layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddendir
 mkdir layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddendir
-setfattr -n trusted.overlay.opaque -v y layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddendir
+setfattr -n trusted.overlay.opaque -v y layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddendir
+
+mknod layer1-dc14934a7f66ff36aaa4de4894679177ab787cb81d8f38ddb40f6de9cd6ba58b/hiddendir2 c 0 0
+mkdir layer2-5091fe3751e44d94ca306eefc31888b877cf818a9f09a30d46cc09c32c03299a/hiddendir2
 
 mkdir -p target
 mkdir -p work
 mkdir -p upper
 mount -t overlay overlay "-olowerdir=${lowerdirs},upperdir=upper,workdir=work" target
-rmdir upper
 
 for i in $(seq ${levels}); do
     stat target/${i} || (echo "${i} missing" && exit 1)
@@ -46,11 +55,12 @@ done
 
 # check our special whiteouts
 [ ! -f target/hiddenfile ]
-[ ! -d target/hiddendir ]
+[ ! -d target/hiddendir2 ]
+[ -d target/hiddendir ] # we expect opaque dirs aren't hidden in lowerdirs
 
 touch target/writable
 
 ls -alh target
 ls -alh work
 ls -alh upper
-stat work/writable
+RESULT=success
